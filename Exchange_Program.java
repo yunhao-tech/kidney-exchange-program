93a8323f3b66f4bf831890c4f46abb3734b97f95
yunhao-tech <77528839+yunhao-tech@users.noreply.github.com>
2021-01-31 20:05:31 +0100

    Delete Exchange_Program.java

--- Exchange_Program.java
+++
@@ -1,8 +0,0 @@
-public class Exchange_Program {
-
-	//initialize all the kidneys and patients
-	//indicate the number of patient-kidney pairs, which is public all over the program
-	//the waiting list is also unique.
-	
-	
-}
